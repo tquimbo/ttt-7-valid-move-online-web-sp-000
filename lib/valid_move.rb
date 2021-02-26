4a543a85c44a61cfca86fed5fa0d605d33441da2
Tim Quimbo <tquimbo1@gmail.com>
2021-02-26 02:11:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken? && board[index].between?(1,8)
     return true
   else
     return false
